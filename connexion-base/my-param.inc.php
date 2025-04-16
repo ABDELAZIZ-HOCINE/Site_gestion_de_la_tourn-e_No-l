c45d6cf4b1489ed75f4b2afdc5c398fe78642622
Abdelaziz HOCINE <abdelazizhocine2842000@gmail.com>
2025-04-16 05:32:48 +0200

    Update my-param.inc.php

--- connexion-base/my-param.inc.php
+++ connexion-base/my-param.inc.php
@@ -1,5 +1,5 @@
 <?php
-  define("MYHOST","10.1.16.56/oracle2");
-  define("MYUSER","pascalrousselle");
-  define("MYPASS","oracle");
+  define("MYHOST","HOST");
+  define("MYUSER","USER");
+  define("MYPASS","DBNAME");
 ?>
